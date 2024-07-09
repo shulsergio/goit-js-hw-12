40ce0650965bac52068826909ad33565692b0df3
Sergey Shulga <shul.sergio@gmail.com>
2024-07-09 19:18:41 +0300

    v 1.2 library install
    v 1.2 library install

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,11 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const paramsPix = {
+  key: '36975970-2726c7257b1c2078714098d3d',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+};
---
+++ src/js/render-functions.js
@@ -0,0 +1,13 @@
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const newGallery = document.querySelector('.main-list');
+
+let userDataLog = new SimpleLightbox('.main-list a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
