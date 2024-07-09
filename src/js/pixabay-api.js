13dfc6f89b5c65569353a231290300ee800ddd5f
Sergey Shulga <shul.sergio@gmail.com>
2024-07-09 21:50:24 +0300

    v 1.3 add js
    v 1.3 add js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -9,3 +9,24 @@ const paramsPix = {
   orientation: 'horizontal',
   safesearch: 'true',
 };
+
+export function onGetPhotoByText(query) {
+  const API_KEY = '36975970-2726c7257b1c2078714098d3d';
+  const targetUrl = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=${paramsPix.image_type}&orientation=${paramsPix.orientation}&safesearch=${paramsPix.safesearch}`;
+  return fetch(targetUrl).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
+export function onFetchError() {
+  iziToast.show({
+    backgroundColor: '#ef4040',
+    message:
+      'Sorry, there are no images matching </br> your search query. Please, try again!',
+    messageColor: '#fff',
+    messageSize: '16px',
+    position: 'topRight',
+  });
+}
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -11,3 +11,56 @@ let userDataLog = new SimpleLightbox('.main-list a', {
   captionPosition: 'bottom',
   captionDelay: 250,
 });
+
+export function onCreateGalleryPhoto(data) {
+  newGallery.innerHTML = '';
+  console.log(data);
+  newGallery.insertAdjacentHTML('beforeend', onCreateData(data.hits));
+  userDataLog.refresh();
+}
+function onCreateData(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="gallery-item">
+            <div class="list-clock">
+              <a class="gallery-link" href=${largeImageURL}>
+                <img
+                  class="gallery-image"
+                  src=${webformatURL}
+                  alt=${tags}
+
+                />
+              </a>
+              <ul class="adds-list">
+                <li class="info-item">
+                  <p class="text-item text-item-name">Likes</p>
+                  <p class="text-item">${likes}</p>
+                </li>
+                <li class="info-item">
+                  <p class="text-item text-item-name">Views</p>
+                  <p class="text-item">${views}</p>
+                </li>
+                <li class="info-item">
+                  <p class="text-item text-item-name">Comments</p>
+                  <p class="text-item">${comments}</p>
+                </li>
+                <li class="info-item">
+                  <p class="text-item text-item-name">Downloads</p>
+                  <p class="text-item">${downloads}</p>
+                </li>
+              </ul>
+            </div>
+          </li>`;
+      }
+    )
+    .join('');
+}
